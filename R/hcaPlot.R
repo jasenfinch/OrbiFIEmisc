ae2d214112824b2e22101201241d954444dd4f97
jasenfinch <jsf9@aber.ac.uk>
2015-06-04 13:57:57 +0100

    package sourcing for pre-processing and hca plotting fixes

--- R/hcaPlot.R
+++ R/hcaPlot.R
@@ -5,7 +5,7 @@ function(dn,dat.all,cls,Path,DF,HPC_mode=F){
  for (i in 1:length(dn)) {
     ldamod <- nlda(dat.all[[i]],cls)
     if (HPC_mode==T){
-    	bitmap(filename = paste(Path,DF,paste(DF,"HCA",sep="_"),paste(paste(DF, paste("lda_hca",dn[i],
+    	bitmap(paste(Path,DF,paste(DF,"HCA",sep="_"),paste(paste(DF, paste("lda_hca",dn[i],
                     "Tw1",as.character(round(ldamod$Tw[[1]]*10, digits=0)/10),
                     "Tw2",as.character(round(ldamod$Tw[[2]]*10, digits=0)/10),
                     sep="_"),
--- R/processDat.R
+++ R/processDat.R
@@ -10,7 +10,7 @@ function(peak.mat,cls,bat,out_id,out_idx,par.m,nCores){
 			nCores <- 2
 		}
     clust = makeCluster(nCores, type="PSOCK")
-    clusterExport(clust,ls("package:FIEmspro"))
+    clusterExport(clust,c(ls("package:FIEmspro"),ls("package:OrbiFIEmisc")))
 		dat.all <- clusterApplyLB(clust,peak.mat, fun=preProcess,cls.1=cls,bat.1=bat,out_id.1=out_id,out_idx.1=out_idx)
     stopCluster(clust)
 	}
