e958c5c587fa8af3d8a70eb22e883e9e99e7286f
jasenfinch <jsf9@aber.ac.uk>
2015-06-10 16:38:48 +0100

    outlier detection mode naming

--- R/outlierDet.R
+++ R/outlierDet.R
@@ -5,16 +5,16 @@ function(dat.all,samp.names,HPC_mode=F){
   dat.p   <- prcomp(dat.all[["Positive_Mode"]],scale=FALSE)$x
   dat.n   <- prcomp(dat.all[["Negative_Mode"]],scale=FALSE)$x
   if (HPC_mode==T){
-  	bitmap(paste(Path,DF,paste(DF,names(dat.all)[1],"outler_detection.bmp",sep="_"),sep="/"))
+  	bitmap(paste(Path,DF,paste(DF,"Positive_Mode","outler_detection.bmp",sep="_"),sep="/"))
   } else {
-  	jpeg(paste(Path,DF,paste(DF,names(dat.all)[1],"outler_detection.jpg",sep="_"),sep="/"))
+  	jpeg(paste(Path,DF,paste(DF,"Positive_Mode","outler_detection.jpg",sep="_"),sep="/"))
   } 
   p <- outDet(dat.p[,c(1,2)],samp.names, method="mcd",dimen=c(1,2),conf.level = 0.975)
   dev.off()
   if (HPC_mode==T){
-  	bitmap(paste(Path,DF,paste(DF,names(dat.all)[2],"outlier_detection.bmp",sep="_"),sep="/"))
+  	bitmap(paste(Path,DF,paste(DF,"Negative_Mode","outlier_detection.bmp",sep="_"),sep="/"))
   } else {
-  	jpeg(paste(Path,DF,paste(DF,names(dat.all)[2],"outlier_detection.jpg",sep="_"),sep="/"))
+  	jpeg(paste(Path,DF,paste(DF,"Negative_Mode","outlier_detection.jpg",sep="_"),sep="/"))
   }
   n <- outDet(dat.n[,c(1,2)],samp.names, method="mcd",dimen=c(1,2),conf.level = 0.975)
   dev.off()
