4bca388d6235bbb39254f7bfb2f034c57b89e34e
jasenfinch <jsf9@aber.ac.uk>
2015-07-20 16:59:13 +0100

    pcaPlot fix

--- R/pcaPlots.R
+++ R/pcaPlots.R
@@ -13,10 +13,10 @@ function(pca.scores,pca.vars.1,PC.1,PC.2,Path,DF,cls.1=NULL,HPC_mode=F){
 				jpeg(paste(Path,DF,paste(DF,"PCA_&_LDA",sep="_"),paste(DF,dn[i],PC.1,PC.2,"PCA.jpg" ,sep="_"),sep="/"))
 			}
 		if (is.null(cls.1)){
-			plot.1 <- ggplot(sub.pca, aes(x = sub.pca[,PC.1], y = sub.pca[,PC.2], colour=sub.pca$y), environment=.e) + 
-				geom_point(size=2,aes(shape=sub.pca$y)) + #
+			plot.1 <- ggplot(sub.pca, aes(x = sub.pca[,PC.1], y = sub.pca[,PC.2], colour=sub.pca$cls), environment=.e) + 
+				geom_point(size=2,aes(shape=sub.pca$cls)) + #
 				#scale_colour_brewer(palette="Dark2") + 
-				scale_shape_manual(values=c(1:length(unique(sub.pca$y)))) + 
+				scale_shape_manual(values=c(1:length(unique(sub.pca$cls)))) + 
 				facet_wrap( ~ type, ncol=2)+ 
 				ggtitle(paste(DF," PCA" ,sep="")) +
 				scale_fill_hue(l=40) +
@@ -25,9 +25,9 @@ function(pca.scores,pca.vars.1,PC.1,PC.2,Path,DF,cls.1=NULL,HPC_mode=F){
 				ylab(paste(PC.2," (Var ",round(pca.vars.1[i,PC.2], digits=2),"%)",sep=""))
 		} else {
 			plot.1 <- ggplot(sub.pca, aes(x = sub.pca[,PC.1], y = sub.pca[,PC.2], colour=cls.1), environment=.e) + 
-				geom_point(size=2,aes(shape=sub.pca$y)) + #
+				geom_point(size=2,aes(shape=sub.pca$cls)) + #
 				#scale_colour_brewer(palette="Dark2") + 
-				scale_shape_manual(values=c(1:length(unique(sub.pca$y)))) + 
+				scale_shape_manual(values=c(1:length(unique(sub.pca$cls)))) + 
 				facet_wrap( ~ type, ncol=2)+ 
 				ggtitle(paste(DF," PCA" ,sep="")) +
 				scale_fill_hue(l=40) +
