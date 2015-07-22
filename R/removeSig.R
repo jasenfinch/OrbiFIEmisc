a233e8d425cf76299c3605cc2a4977f121197dd3
jasenfinch <jsf9@aber.ac.uk>
2015-07-22 14:22:11 +0100

    removeSig mode names fix

--- R/removeSig.R
+++ R/removeSig.R
@@ -6,10 +6,10 @@ function(peak.mat,out_path){ # remove top ranked signals
                      na.strings = "", stringsAsFactors=F, head=T)
   fs_out <- lapply(as.list(fs_out), function(x) x[!is.na(x)])
   fs_out_pos <- fs_out[[1]]
-  idx  <- !(colnames(peak.mat$pos) %in% fs_out_pos)
-  peak.mat$pos <- peak.mat$pos[,idx,drop=F]
+  idx  <- !(colnames(peak.mat$p) %in% fs_out_pos)
+  peak.mat$p <- peak.mat$p[,idx,drop=F]
   fs_out_neg <- fs_out[[2]]
-  idx  <- !(colnames(peak.mat$neg) %in% fs_out_neg)
-  peak.mat$neg <- peak.mat$neg[,idx,drop=F]
+  idx  <- !(colnames(peak.mat$n) %in% fs_out_neg)
+  peak.mat$n <- peak.mat$n[,idx,drop=F]
 	return(peak.mat)
 }
