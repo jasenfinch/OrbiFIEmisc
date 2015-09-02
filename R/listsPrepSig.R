0b4d2b1ae783ecf60d178cad151dd7c9b70a21b6
jasenfinch <jsf9@aber.ac.uk>
2015-09-02 14:39:50 +0100

    feature list processing for new fs.res structure

--- R/listsPrepSig.R
+++ R/listsPrepSig.R
@@ -2,11 +2,17 @@
 
 listsPrepSig <-
 function(fs.res,meth,sel,cut){
-	stats <- fsTab(fs.res)
-	stats <- lapply(stats,selectMethod,meth=meth)
+  stats <- lapply(fs.res,as.data.frame,stringsAsFactors=F)
+  stats <- lapply(stats,function(x,m){x[grep(m,names(x))]},m=meth)
 	if(length(sel)>0){
 	stats <- lapply(stats,listsSpec,sel=sel)
 	}
+  stats <- lapply(stats,function(x){
+    if (length(grep('pvalue',names(x)))>0){
+      x <- x[,-grep('pvalue',names(x))]
+    }
+    return(x)
+  })
 	stats <- lapply(stats,listsSig,thres=cut,meth=meth)
 	stats <- lapply(stats,function(x){return(x[,seq(1,ncol(x),2)])})
 	names(stats) <- names(fs.res)
