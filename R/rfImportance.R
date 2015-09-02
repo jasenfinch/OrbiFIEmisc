623f5b45afd30e8b2e99697b66e1442ab7ed9adb
jasenfinch <jsf9@aber.ac.uk>
2015-09-02 13:50:45 +0100

    changed rfImportance plots for new fs.res layout

--- R/rfImportance.R
+++ R/rfImportance.R
@@ -2,13 +2,12 @@
 
 rfImportance <-
 function(fs.res,sel,Path,DF,meth="rf",HPC_mode=F){  	
-  stats <- fsTab(fs.res)
-  stats <- lapply(stats,selectMethod,meth=meth)
+  stats <- lapply(fs.res,as.data.frame,stringsAsFactors=F) 
+  stats <- lapply(stats,function(x,m){x[grep(m,names(x))]},m=meth)
   if (length(sel) >0){
     stats <- lapply(stats,listsSpec,sel=sel)
   }
-  stats <- lapply(stats,listsOrder)
-  stats <- lapply(stats,function(x){return(x[,seq(2,ncol(x),2)])})
+  stats <- lapply(stats,function(x){return(x[grep('score',names(x))])})
   for (i in 1:length(stats)){
     rfImportancePlot(stats[[i]],names(fs.res)[i],Path,DF,HPC_mode=HPC_mode)
   }
