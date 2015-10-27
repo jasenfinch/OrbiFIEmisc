4d976408af160d296620950ffdbea8b1e1bdcb03
jasenfinch <jsf9@aber.ac.uk>
2015-10-27 17:11:48 +0000

    file name for less than 3 classes

--- R/hcaPlot.R
+++ R/hcaPlot.R
@@ -5,17 +5,17 @@ function(dn,dat.all,cls,Path,DF,HPC_mode=F){
  for (i in 1:length(dn)) {
     ldamod <- nlda(dat.all[[i]],cls)
     if (HPC_mode==T){
-    	bitmap(paste(Path,DF,paste(DF,"HCA",sep="_"),paste(paste(DF, paste("lda_hca",dn[i],
-                    "Tw1",as.character(round(ldamod$Tw[[1]]*10, digits=0)/10),
-                    "Tw2",as.character(round(ldamod$Tw[[2]]*10, digits=0)/10),
-                    sep="_"),
-                    sep="_"),".bmp",sep=""),sep="/"))
+      if (length(unique(cls))<3){
+        bitmap(paste(Path,DF,paste(DF,"HCA",sep="_"),paste(paste(DF, paste("lda_hca",dn[i],"Tw1",as.character(round(ldamod$Tw[[1]]*10, digits=0)/10),sep="_"),sep="_"),".bmp",sep=""),sep="/"))
+      } else {
+        bitmap(paste(Path,DF,paste(DF,"HCA",sep="_"),paste(paste(DF, paste("lda_hca",dn[i],"Tw1",as.character(round(ldamod$Tw[[1]]*10, digits=0)/10),"Tw2",as.character(round(ldamod$Tw[[2]]*10, digits=0)/10),sep="_"),sep="_"),".bmp",sep=""),sep="/"))
+      }
     } else {
-    	png(filename = paste(Path,DF,paste(DF,"HCA",sep="_"),paste(paste(DF, paste("lda_hca",dn[i],
-                    "Tw1",as.character(round(ldamod$Tw[[1]]*10, digits=0)/10),
-                    "Tw2",as.character(round(ldamod$Tw[[2]]*10, digits=0)/10),
-                    sep="_"),
-                    sep="_"),".png",sep=""),sep="/"))
+      if (length(unique(cls))<3){
+        png(filename = paste(Path,DF,paste(DF,"HCA",sep="_"),paste(paste(DF, paste("lda_hca",dn[i],"Tw1",as.character(round(ldamod$Tw[[1]]*10, digits=0)/10),sep="_"),sep="_"),".png",sep=""),sep="/"))
+      } else {
+    	  png(filename = paste(Path,DF,paste(DF,"HCA",sep="_"),paste(paste(DF, paste("lda_hca",dn[i],"Tw1",as.character(round(ldamod$Tw[[1]]*10, digits=0)/10),"Tw2",as.character(round(ldamod$Tw[[2]]*10, digits=0)/10),sep="_"),sep="_"),".png",sep=""),sep="/"))
+      }
     }
     hca.nlda(ldamod)
     paste("Tw1",as.character(round(ldamod$Tw[[1]]*10, digits=0)/10), sep="_")
