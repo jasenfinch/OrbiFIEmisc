55677eb8b795260d8340b20870e2233668190452
jasenfinch <jsf9@aber.ac.uk>
2015-10-30 10:35:21 +0000

    pca results no longer factors

--- R/pcAnalysis.R
+++ R/pcAnalysis.R
@@ -4,14 +4,14 @@ pcAnalysis <- function(dat.all,cls){
 	dn <- names(dat.all)
 	pca.1 <- lapply(dn, function(x) {
      pca  <- pcaComp(dat.all[[x]], scale=F, pcs=1:8)
-   	 scores <- cbind(pca$scores, cls=cls,type=rep(x, nrow(dat.all[[x]])))
-   	 loadings <- cbind(pca$loadings, type=rep(x, nrow(pca$loadings)))
+   	 scores <- data.frame(pca$scores, cls=cls,type=rep(x, nrow(dat.all[[x]])))
+   	 loadings <- data.frame(pca$loadings, type=rep(x, nrow(pca$loadings)))
    	 list(scores=scores, vars=pca$vars,loadings=loadings)
   })
     pca.scores <- do.call(rbind, lapply(pca.1, function(x) x$scores))
     pca.vars   <- do.call(rbind, lapply(pca.1, function(x) x$vars))
     rownames(pca.vars) <- dn
-    pca.loadings <- do.call(rbind, lapply(pca.1, function(x) x$loadings))
+    pca.loadings <- ldply(lapply(pca.1, function(x){return(x$loadings)}),data.frame)
     pca.res <- list(pca.scores=pca.scores, pca.vars=pca.vars, pca.loadings=data.frame(pca.loadings,stringsAsFactors = F))
 	return(pca.res)
 }
