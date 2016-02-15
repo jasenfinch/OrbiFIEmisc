cd6755ad0de5e7a9a90c17568e7f3824e334b948
jasenfinch <jsf9@aber.ac.uk>
2016-02-15 16:51:20 +0000

    reduced computation time for fs.rf2

--- R/fs.rf2.R
+++ R/fs.rf2.R
@@ -17,20 +17,23 @@
     Imp <- Importance(model, x)
     kval <- round(mean(apply(model$forest$nodestatus,2,function(x){length(which(x==1))})),0)
   
-    meas <- Imp$SF$Frequency
-    names(meas) <- Imp$SF$Feature
+    meas <- Imp$SF
     
-    fs.rank <- rank(-meas, na.last=T, ties.method="random")
+    fs.rank <- rank(-meas$Frequency, na.last=T, ties.method="random")
+    names(fs.rank) <- meas$Feature
+    
     fs.order <- order(fs.rank, na.last=T)
     
-    
-    names(fs.rank) <- names(meas)
-    nam <- names(meas[fs.order])
+    nam <- meas$Feature[fs.order]
     if (!is.null(nam))
       fs.order <- noquote(nam)
+
+    FPR <- sapply(sort(unique(meas$Frequency)),selectionFrequencyFPR,K=kval,Tr=nTree,Ft=nrow(meas))
+    FPR.pos <- match(meas$Frequency,sort(unique(meas$Frequency)))
+    for (i in 1:length(FPR)){
+      FPR.pos[which(FPR.pos==i)] <- FPR[i]
+    }
     
-    FPR <- sapply(meas,selectionFrequencyFPR,kval,nTree,length(meas))
-    
-    res <- list(fs.rank=fs.rank, fs.order=fs.order, stats=meas,pval=FPR)
+    res <- list(fs.rank=fs.rank, fs.order=fs.order, stats=meas,pval=FPR.pos)
     return(res)
   }
